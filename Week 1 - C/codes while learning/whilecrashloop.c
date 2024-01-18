407b3c80f43d9942ce20a4b2b9c19deb01afeb22
Carlos Santos <you@example.com>
2024-01-17 21:40:53 -0300

    more codes while learning

---
+++ Week 1 - C/codes while learning/whilecrashloop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    while (true)
+    {
+        printf("Meow\n");
+    }
+}
--- Week 1 - C/testes.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main(void)
-{
-    char answer = get_char("Type (y) for yes and (n) for no: ");
-    printf("Your choice was: %c", answer);
-}
